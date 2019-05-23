5f8101513bd19844a5fdc11c8ca187c4129fc74c
Alexander Rowland <arowland900@gmail.com>
2019-05-23 14:47:56 -0700

    add day 1 solution

--- answers.js
+++ answers.js
@@ -1,3 +1,28 @@
-function test(){
-    return "test"
+// QUESTION 1
+
+function pickPeaks(arr) {
+
+    var obj = {}
+    obj.pos = []
+    obj.peaks = []
+
+    arr.forEach((e, i) => {
+        if (i > 0 && i < arr.length - 2) {
+            if (e > arr[i - 1] && e > arr[i + 1]) {
+                obj.pos.push(i)
+                obj.peaks.push(e)
+            }
+            else if ((e > arr[i - 1] && e == arr[i + 1])) {
+                var j = 2
+                while (arr[i + 1] == arr[i + j]) {
+                    j++
+                }
+                if (arr[i + 1] > arr[i + j]) {
+                    obj.pos.push(i)
+                    obj.peaks.push(e)
+                }
+            }
+        }
+    })
+    return obj
 }
